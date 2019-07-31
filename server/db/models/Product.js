0334fb566185030fd0b6a84cb4e29135cf4d214f
katherinepeterson <katherine.peterson@yahoo.com>
2019-07-31 15:53:07 -0700

    product model

---
+++ server/db/models/Product.js
@@ -0,0 +1,24 @@
+const db = require('../index');
+const Sequelize = require('sequelize');
+
+const Product = db.define('product', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+    },
+    name: {
+        type: Sequelize.STRING,
+    },
+    price: {
+        type: Sequelize.DECIMAL,
+    },
+    inventory: {
+        type: Sequelize.NUMBER,
+    },
+});
+
+module.exports = Product;
