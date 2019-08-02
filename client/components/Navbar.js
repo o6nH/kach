4116b6cae1dc902d3634f9bf93cd6bc53aff66fe
CB6670 <connerstennett@gmail.com>
2019-08-01 20:29:22 -0700

    added some more route tests

---
+++ client/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const marketName = 'Generic';
+const marketLogo = '../../public/img/bottle_64.png';
+const cartImg = '../../public/img/cart_64.png';
+
+// Fake Store (TODO: substitute fake store for redux store)
+const fakeUser = {userId: '123', isAuthenticated: true};
+const cartOrder = {orderId:'ord123', productCount: 5};
+
+// Component
+const Navbar = () => {
+  const {userId, isAuthenticated: isAuth} = fakeUser;
+  const {orderId:cartOrderId, productCount:cartProdCount} = cartOrder;
+  return (
+    // TODO: remove inline styles
+    <div style={{display:'flex', justifyContent:'space-between'}}>
+      <Link to='/'>
+        <div style={{display:'flex'}}>
+          <img src={marketLogo} alt="Generic Logo"/>
+          <h1>{`${marketName}`}</h1>
+        </div>
+      </Link>
+      <ul style={{display:'flex', width:'30%', justifyContent:'space-between', listStyle:'none'}}>
+        <li><Link to='/products'>Products</Link></li>
+        {
+          isAuth
+          ? <li><Link to={`/users/${userId}`}>Manage Account</Link></li>
+          : <li><Link to='/signup'>Create Account</Link></li> 
+        }
+        {
+          isAuth
+          ? <li onClick={()=>{console.log('Need to sign user out')}}><Link to='/'>Sign Out</Link></li> 
+          : <li><Link to='/signin'>Sign In</Link></li>
+        }
+        <li><Link to={`/orders/${cartOrderId}`}><img src={cartImg}></img>{cartProdCount}</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
