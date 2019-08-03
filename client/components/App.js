2160013e9ce60e3a7b0fbae8dc6b5758721c338c
Hugo <contacthugocampos@gmail.com>
2019-08-02 22:31:10 -0700

    filter products by category

--- client/components/App.js
+++ client/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {HashRouter, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Products from './Products';
+import Product from './Product';
 
 export default class App extends React.Component{
   render(){
@@ -9,8 +10,8 @@ export default class App extends React.Component{
       <HashRouter>
         <Route path='/' component={Navbar}/>
         <Route exact path='/products' component={Products}/>
-        {/* <Route path='/products/:productId' component={Product}/>
-        <Route exact path='/users' component={Users}/> //admin only
+        <Route path='/products/:productId' component={Product}/>
+        {/* <Route exact path='/users' component={Users}/> //admin only
         <Route exact path='/users/:userId' component={User}/>
         <Route exact path='/orders' component={Orders}/>
         <Route exact path='/orders/:orderId' component={Order}/>
