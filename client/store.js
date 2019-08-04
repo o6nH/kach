560050fc3d9ab4b61df4e7a332381c90e8b2729f
Hugo <contacthugocampos@gmail.com>
2019-08-04 16:57:44 -0700

    init redux store

---
+++ client/store.js
@@ -0,0 +1,67 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import loggingMiddleware from 'redux-logger';
+import axios from 'axios';
+
+//Actions
+const ACT = {
+  //ACT_1:...
+
+}
+
+//HelperFunction
+
+//Creators (Action or Thunk)
+
+
+//Reducers
+const userReducer = (state={}, action) {
+  switch (action) {
+    case value:
+      return;
+  
+    default:
+      return state;
+  }
+};
+
+const usersReducer = (state=[], action) {
+  switch (action) {
+    case value:
+      return;
+  
+    default:
+      return state;
+  }
+}; //isAuth ? allUsers : null
+
+const ordersReducer = (state=[], action) {
+  switch (action) {
+    case value:
+      return;
+  
+    default:
+      return state;
+  }
+}; //userId => [{orderId: ..., status:'inCart'}, {}]
+
+const productsReducer = (state = [], action) => {
+  switch (action.type) {
+    case value:
+      
+      return
+    default:
+      return state;
+  }
+};
+
+//Store
+export default createStore(
+  combineReducers({
+    user: userReducer,
+    users: usersReducer,
+    orders: ordersReducer,
+    products: productsReducer, //TODO: refactor to limit number of products downloaded
+  }),
+  applyMiddleware(loggingMiddleware, thunkMiddleware.withExtraArgument(axios))
+);
