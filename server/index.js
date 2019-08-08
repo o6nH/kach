2c3a12f07105eb76c5948083cd3ff015e0b04320
CB6670 <connerstennett@gmail.com>
2019-08-08 16:41:12 -0700

    get connect-session-sequelize associated w/ table

--- server/index.js
+++ server/index.js
@@ -3,27 +3,26 @@ const app = express();
 const path = require('path');
 const routes = require('./routes/index');
 const session = require('express-session');
-require('dotenv').config();
-const sessionDB = require('./db/index');
-const port = process.env.PORT || 3000;
+const db = require('./db/index');
+const sessionModel = require('./db/models/Session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 const store = new SequelizeStore({
-  db: sessionDB,
-  table: 'sessions',
+  db,
+  table: 'session',
   extendDefaults: (defaults, _session) => {
     return {
       data: defaults.data,
       expires: defaults.expires,
       userId: _session.userId,
-      isAuthenticated: _session.isAuthenticated
     }
   }
 })
-console.log(store);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '..', 'public')))
 app.use('/api', routes);
 app.use(session({
   secret: 'a;ldf;alskdf',
@@ -31,8 +30,9 @@ app.use(session({
   saveUninitialized: true,
   store
 }))
-
 app.listen(port, () => console.log(`listening on port ${port}`));
+
+db.sync();
 
 module.exports = {
     app,
