e52c38d955f681cf23b95d837242f2583c81c97d
CB6670 <connerstennett@gmail.com>
2019-08-07 22:04:40 -0700

    edited routes/index.js

--- server/index.js
+++ server/index.js
@@ -4,48 +4,31 @@ const path = require('path');
 const routes = require('./routes/index');
 const session = require('express-session');
 require('dotenv').config();
+const sessionDB = require('./db/index');
 const port = process.env.PORT || 3000;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sessionDB = require('./db/index');
 
-function extendDefaultFields(defaults, _session){
-  return {
-    data: defaults.data,
-    expires: defaults.expires,
-    userId: _session.userId,
-    isAuthenticated: _session.isAuthenticated,
-  }
-}
-
-const sequelizeSessionStore = new SequelizeStore({
+const store = new SequelizeStore({
   db: sessionDB,
-  sessionModel: 'sessions'
+  table: 'sessions',
+  extendDefaults: (defaults, _session) => {
+    return {
+      data: defaults.data,
+      expires: defaults.expires,
+      userId: _session.userId,
+      isAuthenticated: _session.isAuthenticated
+    }
+  }
 })
-
-console.log("ssstore", sequelizeSessionStore);
-
-let sess = {
-  secret: process.env.SECRET || 'Darn cool secret!',
-  store: sequelizeSessionStore,
-  // checkExpirationInterval: 15 * 60 * 1000,
-  // expiration: 90 * 24 * 60 * 60 * 1000,
-  resave: false,
-  saveUninitialized: true,
-  // cookie: {}
-}
-
-// if (app.get('env') === 'production') {
-//   app.set('trust proxy', 1) // trust first proxy
-//   sess.cookie.secure = true // serve secure cookies
-// }
-
+console.log(store);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(session(sess))
-app.use('/api', routes);
-app.use('/', express.static(path.join(__dirname, '../public')));
-
-sequelizeSessionStore.sync();
+app.use(session({
+  secret: 'a;ldf;alskdf',
+  resave: false,
+  saveUninitialized: true,
+  store
+}))
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
