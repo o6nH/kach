7ece4a56d0a9007df8e5d2513f3ea37d8c1f23f5
CB6670 <connerstennett@gmail.com>
2019-08-08 16:44:53 -0700

    get user authentication route for front end

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../db/models/User');
 const productsRoutes = require('./productRoutes');
 const userRoutes = require('./userRoutes');
+const { User } = require('../db/models/index');
 
-/* router.get('/', async (req, res, next) => {
+router.get('/sessions', async (req, res, next) => {
     try {
         const userId = req.session.userId
-        if(!userId){
-            user.createGuest(req.session.id)
-        }
-        })
-        if (user){
-            res.send(user);
+        if (!userId){
+            const guestUser = await User.createGuest(req.session.id)
+            req.session.userId = guestUser.id;
+            res.status(201).send("User Created!");
         } else {
-            res.send(req.session);
+            const user = await User.findOne({
+                where: {
+                    id: userId
+                }
+            })
+
+            const sendObj = {
+                id: user.id,
+                firstName: user.firstName,
+                
+            }
+
+            res.send()
         }
         next();
     } catch (err){
         console.error(err);
     }
-}) */
+}) 
 
 router.use('/products', productsRoutes);
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router
