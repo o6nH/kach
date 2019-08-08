d9d7215a6572a9f0c835263619c811aa73764de3
CB6670 <connerstennett@gmail.com>
2019-08-07 21:58:21 -0700

    edited hash file

--- script/hash.js
+++ script/hash.js
@@ -1,11 +1,13 @@
 const crypto = require('crypto');
+require('dotenv').config();
 
-const secret = process.env.SECRET || 'SuperSecretSecret';
+const secret = process.env.SECRET || 's3cret';
 
-const hash = (password) => 
-    crypto.createHmac('sha256', secret)
+const hash = (password) => {
+    return (crypto.createHmac('sha256', secret)
         .update(password)
-        .digest('hex');
+        .digest('hex'))
+}
 
 
 module.exports = hash;
