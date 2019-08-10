3424e6afa5b8b8d2dcca8496e85f634c5ac4aff5
Hugo <contacthugocampos@gmail.com>
2019-08-09 22:21:39 -0700

    show more dummy data

--- client/store.js
+++ client/store.js
@@ -80,7 +80,19 @@ export const fetchSelectedProduct = (productId) => (dispatch, getState, axios) =
 //Reducers
 
 //TODO: create function to set current user on the store
-const userReducer = (state={id: '058007a1-144e-4b42-96fe-1a59482b9520', isAdmin: true}, action) => {
+const userReducer = (state = {
+    id: '058007a1-144e-4b42-96fe-1a59482b9520', 
+    firstName:'Hugo',
+    lastName: 'Campos', 
+    streetAddress: '123 Fake St', 
+    suite: 'A', 
+    city: 'San Luis Obispo', 
+    state: 'CA', 
+    zip: '92555', 
+    email: 'HugoCampos@email.com', 
+    isAdmin: true, 
+    isAuthenticated: true
+  }, action) => {
   switch (action.type) {
     case ACT:
       return;
@@ -100,7 +112,7 @@ const usersReducer = (state=[], action) => {
   }
 }; //isAuth ? allUsers : null
 
-const ordersReducer = (state=[{id: 'ord123', status:'inCart'}], action) => {
+const ordersReducer = (state=[{id:'ord123', status:'inCart'}], action) => {
   switch (action.type) {
     case ACT:
       return;
