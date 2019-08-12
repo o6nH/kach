446f5ec9fc18e9432617d2d86a152fab9f58a050
katherinepeterson <katherine.peterson@yahoo.com>
2019-08-11 21:33:44 -0700

    checkout fields autofill with info we have from current user

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -19,6 +19,20 @@ class Checkout extends Component {
          this.onChange = this.onChange.bind(this);
     }
 
+    componentDidMount() {
+        const { firstName, lastName, streetAddress, suite, city, state, zip, email } = this.props.user;
+        this.setState({
+            firstName: firstName,
+            lastName: lastName,
+            streetAddress: streetAddress,
+            suite: suite,
+            city: city,
+            state: state,
+            zip: zip,
+            email: email,
+        });
+    }
+
     onSubmit(ev) {
         ev.preventDefault();
         this.props.checkout(this.state);
@@ -27,31 +41,34 @@ class Checkout extends Component {
 
     onChange(ev) {
         this.setState({[ev.target.name]: ev.target.value});
+        console.log(this.state)
     }
 
     render() { 
         const { onChange, onSubmit } = this;
+        console.log(this.props)
+        const { firstName, lastName, streetAddress, suite, city, state, zip, email } = this.state;
         return ( 
             <div>
                 <h1>Checkout</h1>
                     <form type='submit' onSubmit={onSubmit}>
                         <h3>Shipping Address</h3>
                         <label>First Name: </label> <br/>
-                        <input type='text' name='firstName' onChange={onChange}></input><br/>
+                        <input type='text' name='firstName' onChange={onChange} value={firstName}></input><br/>
                         <label>Last Name: </label><br/>
-                        <input type='text' name='lastName' onChange={onChange}></input><br/>
+                        <input type='text' name='lastName' onChange={onChange} value={lastName}></input><br/>
                         <label>Street Address: </label> <br/>
-                        <input type='text' name='streetAddress' onChange={onChange}></input><br/>
+                        <input type='text' name='streetAddress' onChange={onChange} value={streetAddress}></input><br/>
                         <label>Suite: </label><br/>
-                        <input type='text' name='suite' onChange={onChange}></input><br/>
+                        <input type='text' name='suite' onChange={onChange} value={suite}></input><br/>
                         <label>City: </label> <br/>
-                        <input type='text' name='city' onChange={onChange}></input><br/>
+                        <input type='text' name='city' onChange={onChange} value={city}></input><br/>
                         <label>State: </label><br/>
-                        <input type='text' name='state' onChange={onChange}></input><br/>
+                        <input type='text' name='state' onChange={onChange} value={state}></input><br/>
                         <label>Zip Code: </label><br/>
-                        <input type='text' name='zip' onChange={onChange}></input><br/>
+                        <input type='text' name='zip' onChange={onChange} value={zip}></input><br/>
                         <label>Email: </label> <br/>
-                        <input type='text' name='email' onChange={onChange}></input><br/>
+                        <input type='text' name='email' onChange={onChange} value={email}></input><br/>
                         <br/>
                         <button type='submit'>Place Order</button>
                     </form>
@@ -61,6 +78,7 @@ class Checkout extends Component {
     }
 
 const mapStateToProps = state => ({
+    user: state.user,
 })
 
 const mapDispatchToProps = dispatch => {
