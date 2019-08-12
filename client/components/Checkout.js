ead59f8576b01fb240b5162fb249594066c76104
katherinepeterson <katherine.peterson@yahoo.com>
2019-08-11 21:06:43 -0700

    add to cart decreases quantity available in inventory

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { checkout } from '../store';
@@ -71,64 +70,3 @@ const mapDispatchToProps = dispatch => {
 };
  
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
-=======
-import React, { Component } from 'react';
-
-class Checkout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            firstName: '',
-            lastName: '',
-            streetAddress: '',
-            suite: '',
-            city: '',
-            state: '',
-            zip: '',
-            email: '',
-         }
-         this.onSubmit = this.onSubmit.bind(this);
-         this.onChange = this.onChange.bind(this);
-    }
-
-    onSubmit(ev) {
-        ev.preventDefault();
-    }
-
-    onChange(ev) {
-        this.setState({[ev.target.name]: ev.target.value});
-    }
-
-    render() { 
-        const { onChange, onSubmit } = this;
-        return ( 
-            <div>
-                <h1>Checkout</h1>
-                    <form type='submit' onSubmit={onSubmit}>
-                        <h3>Shipping Address</h3>
-                        <label>First Name: </label> <br/>
-                        <input type='text' name='firstName' onChange={onChange}></input><br/>
-                        <label>Last Name: </label><br/>
-                        <input type='text' name='lastName' onChange={onChange}></input><br/>
-                        <label>Street Address: </label> <br/>
-                        <input type='text' name='streetAddress' onChange={onChange}></input><br/>
-                        <label>Suite: </label><br/>
-                        <input type='text' name='suite' onChange={onChange}></input><br/>
-                        <label>City: </label> <br/>
-                        <input type='text' name='city' onChange={onChange}></input><br/>
-                        <label>State: </label><br/>
-                        <input type='text' name='state' onChange={onChange}></input><br/>
-                        <label>Zip Code: </label><br/>
-                        <input type='text' name='zip' onChange={onChange}></input><br/>
-                        <label>Email: </label> <br/>
-                        <input type='text' name='email' onChange={onChange}></input><br/>
-                        <br/>
-                        <button type='submit'>Place Order</button>
-                    </form>
-            </div>
-         );
-        }
-    }
- 
-export default Checkout;
->>>>>>> 894e907261d75ffd34054add9d5bc0f2c0e3c895
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const router = require('express').Router();
 const Order = require('../db/models/Order');
 const Product = require('../db/models/Product');
@@ -45,6 +44,11 @@ router.route('/')
                 
             }
             const productFromLine = await Product.findByPk(newLine.productId)
+            console.log('PRODUCT FROM LINE', productFromLine)
+            
+            productFromLine.quantity--;
+            await productFromLine.save();
+            
             newLine.dataValues.product = productFromLine;
             res.send(newLine)
 
@@ -167,139 +171,4 @@ router.route('/checkout')
         }
     });
 
-=======
-const router = require('express').Router();
-const Order = require('../db/models/Order');
-const Product = require('../db/models/Product');
-const OrderProduct = require('../db/models/OrderProduct');
-
-router.route('/')
-    .post(async (req, res, next) => {
-        try {
-            let currentCart = await Order.findOrCreate(
-                    {
-                        where: {
-                                userId: '058007a1-144e-4b42-96fe-1a59482b9520',
-                                status: 'inCart'
-                            },
-                    }
-                );
-            currentCart = currentCart[0].dataValues;
-            const orderLine = await OrderProduct.findAll({
-                where: {
-                    productId: req.body.id,
-                    orderId: currentCart.id
-                }
-            });
-            let newLine = {};
-            if (orderLine && orderLine[0]) {
-                orderLine[0].dataValues.quantity++;
-                [,[newLine]] = await OrderProduct.update(orderLine[0].dataValues,
-                    {
-                        where: {
-                            id: orderLine[0].dataValues.id
-                        },
-                        returning: true
-                    }) 
-            } else {
-                newLine = await OrderProduct.create(
-                    {
-                        productId: req.body.id,
-                        orderId: currentCart.id,
-                        purchaseUnitPrice: req.body.price,
-                        quantity: 1
-                    }
-                )
-                
-            }
-            const productFromLine = await Product.findByPk(newLine.productId)
-            newLine.dataValues.product = productFromLine;
-            res.send(newLine)
-
-        } catch (err){
-            console.error(err);
-        }
-    })
-    .delete(async (req, res, next) => {
-        try {
-            let [currentCart] = await Order.findAll(
-                {
-                    where: {
-                            userId: '058007a1-144e-4b42-96fe-1a59482b9520',
-                            status: 'inCart'
-                        },
-                }
-            );
-            currentCart = currentCart.dataValues;
-
-            let [orderLine] = await OrderProduct.findAll({
-                where: {
-                    productId: req.body.id,
-                    orderId: currentCart.id
-                }
-            });
-
-            orderLine = orderLine.dataValues;
-            
-            orderLine.quantity--;
-
-            let changedLine = {};
-
-            if (!orderLine.quantity) {
-                changedLine = await OrderProduct.destroy({
-                    where: {
-                        productId: req.body.id,
-                        orderId: currentCart.id
-                    },
-                    returning: true
-                })
-                res.send(orderLine);
-            } else {
-                [,[changedLine]] = await OrderProduct.update(orderLine,
-                    {
-                        where: {
-                            id: orderLine.id
-                        },
-                        returning: true
-                    }) 
-                changedLine = changedLine.dataValues;
-                const productFromLine = await Product.findByPk(changedLine.productId)
-                changedLine.product = productFromLine.dataValues;
-                res.send(changedLine)
-            }
-            
-        } catch (err) {
-            console.log(err);
-        }
-    })
-
-router.route('/cart')
-    .get(async (req, res, next) => {
-        try {
-            //TODO: bring in real userId
-            let currentCart = await Order.findOrCreate(
-                {
-                    where: {
-                            userId: '058007a1-144e-4b42-96fe-1a59482b9520',
-                            status: 'inCart'
-                        },
-                }
-            );
-            currentCart = currentCart[0].dataValues;
-
-            let orderLines = await OrderProduct.findAll({
-                where: {
-                    orderId: currentCart.id
-                },
-                include: {model: Product}
-            });
-            //orderLines = orderLines.dataValues;
-            console.log('ORDER LINES: ', orderLines)
-            res.send(orderLines);
-        } catch (err){
-            console.error(err);
-        }
-    });
-
->>>>>>> 894e907261d75ffd34054add9d5bc0f2c0e3c895
 module.exports = router;
