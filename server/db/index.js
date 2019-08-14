c6bd67fdaa1d8054a290be303b32c5171d1aff86
katherinepeterson <katherine.peterson@yahoo.com>
2019-08-13 21:06:31 -0700

    .

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,4 @@
 const db = require('./connection');
 const {Session, User, Order, OrderProduct, Product} = require('./models/index');
-
-
-
   
-  module.exports = {db, Session, User, Order, OrderProduct, Product};
+module.exports = {db, Session, User, Order, OrderProduct, Product};
