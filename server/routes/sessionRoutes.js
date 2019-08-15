6327a21e50779898cd56503dcc6f96b6cb861c74
CB6670 <connerstennett@gmail.com>
2019-08-15 12:27:19 -0700

    console.logs cleared

--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -3,13 +3,10 @@ const { User } = require('../db/models/index');
 
 router.use('/', async (req, res, next) => {
     try {
-        console.log(req.session.userId)
         if (!req.session.userId){
             const guestUser = await User.create();
             req.session.userId = guestUser.id;
         }
-
-        console.log(req.session.userId);
         next();
     } catch (err){
         throw err;
