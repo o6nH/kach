63a73ba4a39d758447d443dd4e236131930991df
Hugo <contacthugocampos@gmail.com>
2019-08-15 07:50:33 -0700

    make Products component a class with methods

--- client/components/Products.js
+++ client/components/Products.js
@@ -5,53 +5,63 @@ import CategoriesFilter from './CategoriesFilter';
 import SearchForm from './SearchForm';
 import ProductCard from './ProductCard';
 
-function Products(props) {
-  const {products, categorizedProducts, categories} = props;
-
+class Products extends React.Component{
+  constructor(props){
+    super(props);
+    this.productsByQueryCategory = this.productsByQueryCategory.bind(this);
+    this.productsByQuerySearchTerm = this.productsByQuerySearchTerm.bind(this);
+    this.hasSearchTerm = this.hasSearchTerm.bind(this);
+  };
+  
   //Filter Products by Category
-  const productsByQueryCategory = (categorizedProducts = categorizedProducts, allProducts = products) => {
-    const parsedQuery = props.location.search ? queryString.parse(props.location.search) : {};
+  productsByQueryCategory(categorizedProducts, allProducts) {
+    const {location} = this.props;
+    const parsedQuery = location.search ? queryString.parse(location.search) : {};
     if(parsedQuery['?category'] || parsedQuery['category']) {
       return categorizedProducts[parsedQuery['?category']].products || categorizedProducts[parsedQuery['category']].products;
     }
     return allProducts;
   };
-
+  
   //Filter Products by SearchTerm
-  const hasSearchTerm = (product, searchTerm) =>  {
-    searchTerm = searchTerm && searchTerm.toLowerCase();
-    const splitName = product.name.toLowerCase().split(' ');
-    const splitDescription = product.description.toLowerCase().split(' ');
-    return splitName.includes(searchTerm) || splitDescription.includes(searchTerm)
-  };
-
-  const productsByQuerySearchTerm = (allProducts = products) => {
-    const parsedQuery = props.location.search ? queryString.parse(props.location.search) : {};
+  productsByQuerySearchTerm(allProducts) {
+    const {hasSearchTerm} = this;
+    const {location} = this.props;
+    const parsedQuery = location.search ? queryString.parse(location.search) : {};
     if (parsedQuery['?search'] || parsedQuery['search']) {
-      return products.filter(product => {
+      return allProducts.filter(product => {
         return hasSearchTerm(product, parsedQuery['?search']) || hasSearchTerm(product, parsedQuery['search'])
       });
     }
     return allProducts;
   };
 
-  const filteredProducts = categories.length 
-    ? productsByQuerySearchTerm(productsByQueryCategory(categorizedProducts)) : '';
+  hasSearchTerm(product, searchTerm) {
+    searchTerm = searchTerm && searchTerm.toLowerCase();
+    const splitName = product.name.toLowerCase().split(' ');
+    const splitDescription = product.description.toLowerCase().split(' ');
+    return splitName.includes(searchTerm) || splitDescription.includes(searchTerm);
+  };
   
-  //Component
-  return (/* TODO:remove inline styles*/
-    <div>
-      <SearchForm location={props.location}/>
-      <CategoriesFilter/>
-      <div style={{display:'flex'}}>
-      {
-        categories.length 
-        ? filteredProducts.map(product => <ProductCard key={product.id} product={product}/>) 
-        : ''
-      }
+  render(){
+    const {location, products, categorizedProducts, categories} = this.props;
+    const {productsByQueryCategory, productsByQuerySearchTerm} = this;
+    const filteredProducts = categories.length 
+      ? productsByQuerySearchTerm(productsByQueryCategory(categorizedProducts, products)) : '';
+    return (/* TODO:remove inline styles*/
+      <div>
+        <SearchForm location={location}/>
+        <CategoriesFilter/>
+        <div style={{display:'flex'}}>
+        {
+          categories.length 
+          ? filteredProducts.map(product => <ProductCard key={product.id} product={product}/>) 
+          : ''
+        }
+        </div>
       </div>
-    </div>
-  );
+    )
+  };
 };
 
 //Mappings from redux store to react component's props
