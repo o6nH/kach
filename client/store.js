8c138d2228bea65f006bfee82222dc6e4a368b82
Hugo <contacthugocampos@gmail.com>
2019-08-17 09:12:02 -0700

    remove logs

--- client/store.js
+++ client/store.js
@@ -37,6 +37,8 @@ const productsReducer = (state = [], action) => {
     case ACT.GET_PRODUCTS:
       return action.products
     case ACT.CREATE_PRODUCT:
+      console.log("CREATING PRODUCT in productReducer");
+      
       return [...state, action.product]
     case ACT.DELETE_PRODUCT:
       const {productId} = action
