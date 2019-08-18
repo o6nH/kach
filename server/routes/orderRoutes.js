122a76764e9abe553264d9f97a276b9a63321834
katherinepeterson <katherine.peterson@yahoo.com>
2019-08-17 20:02:08 -0700

    oops now it adds to inventory

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -110,7 +110,7 @@ router.route('/')
                     }) 
                 changedLine = changedLine.dataValues;
                 const productFromLine = await Product.findByPk(changedLine.productId)
-                productFromLine.quantity--;
+                productFromLine.quantity++;
                 await productFromLine.save();
                 changedLine.product = productFromLine.dataValues;
                 res.send(changedLine)
