28ae25828e868cf53d513ad757bc94603ab0bef3
Hugo <contacthugocampos@gmail.com>
2019-08-19 22:45:25 -0700

    add reminder to salt

--- script/hash.js
+++ script/hash.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const secret = process.env.SECRET || 's3cret';
 
-const hash = (password) => {
+const hash = (password) => {//TODO: salt prior to hashing
     return (crypto.createHmac('sha256', secret)
         .update(password)
         .digest('hex'))
