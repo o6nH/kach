6739cdd5eeb8629c671b21daf1040981088f2a63
Hugo <contacthugocampos@gmail.com>
2019-12-04 12:05:58 -0800

    ignore linting erro for process.env

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
+    // eslint-disable-next-line no-undef
     process.env.DATABASE_URL || 'postgres://localhost/kach-grace-shopper',
     { logging: false }
   );
