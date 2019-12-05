b6f5f69591e121d76ee568559b8b9963be280edd
Hugo <contacthugocampos@gmail.com>
2019-12-05 11:20:57 -0800

    update navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Axios from 'axios';
@@ -17,33 +18,45 @@ const Navbar = ({user, cart, getUser}) => {
   const {productCount:cartProdCount} = cart;
   return (
     // TODO: remove inline styles
-    <div>
-      <Link to='/' className="nav">
-        <div style={{display:'flex'}}>
-          <h1 style={{fontSize: '32px'}}>{`💻${marketName}`}</h1>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to='/' className="navbar-brand" style={{fontFamily: 'Monserrat', fontStyle:'bold', fontSize: '32px'}}>
+        {`💻${marketName}`}
       </Link>
-      <ul className="nav justify-content-end nav-fill">
-        <li className="nav-item"><Link to='/products'>Products</Link></li>
-        {
-          isAdmin
-          ? <li className="nav-item"><Link to={`/admin`}>Admin Dashboard</Link></li>
-          : ''
-        }
-        {
-          isAuth
-          ? <li className="nav-item"><Link to={`/users/${userId}`}>Manage Account</Link></li>
-          : <li className="nav-item"><Link to='/signup'>Create Account</Link></li> 
-        }
-        {
-          isAuth
-          ? <li className="nav-item" onClick={()=>{signOut(getUser)}}><Link to='/'>Sign Out</Link></li> 
-          : <li className="nav-item"><Link to='/signin'>Sign In</Link></li>
-        }
-        <li className="nav-item"><Link to={'/cart'} style={{fontSize: '32px'}}>🛒{cartProdCount}</Link></li>
-      </ul>
-    </div>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div id="navbarNav" className="collapse navbar-collapse justify-content-end ">
+        <ul className="navbar-nav">
+          <li className="nav-item table-hover"><Link to='/products' className="nav-link">Products</Link></li>
+          {
+            isAdmin
+            ? <li className="nav-item"><Link to={`/admin`} className="nav-link">Admin Dashboard</Link></li>
+            : ''
+          }
+          {
+            isAuth
+            ? <><li className="nav-item"><Link to={`/users/${userId}`} className="nav-link">Manage Account</Link></li>
+              <li className="nav-item" onClick={()=>{signOut(getUser)}}><Link to='/' className="nav-link">Sign Out</Link></li></>
+            : <><li className="nav-item"><Link to='/signup' className="nav-link">Create Account</Link></li> 
+            <li className="nav-item"><Link to='/signin' className="nav-link">Sign In</Link></li></>
+          }
+        </ul>
+        <div><Link to={'/cart'} style={{fontSize: '32px'}} className="nav-link">🛒{cartProdCount}</Link></div>
+      </div>
+    </nav>
   )
+};
+
+Navbar.propTypes = {
+  getUser: PropTypes.func, 
+  user: PropTypes.shape({
+    id: PropTypes.string,
+    isAuthenticated: PropTypes.bool, 
+    isAdmin: PropTypes.bool,
+  }),
+  cart: PropTypes.shape({
+    productCount: PropTypes.bool
+  })
 }
 
 const mapStateToProps = state => ({
